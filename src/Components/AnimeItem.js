5e1520b337b5c6f2d20984111c16128b2ceebf24
hikiasi <33026229+hikiasi@users.noreply.github.com>
2023-08-20 22:32:41 +0200

    added franchises in anime page

--- src/Components/AnimeItem.js
+++ src/Components/AnimeItem.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { posterUrl } from "../context/global";
 import styled from "styled-components";
 
@@ -8,8 +8,10 @@ const Anime = () => {
 
   //state
   const [anime, setAnime] = React.useState({});
-  const [characters, setCharacters] = React.useState({});
+  const [franchise, setFranchise] = React.useState({});
   const [showMore, setShowMore] = React.useState(false);
+  const navigate = useNavigate();
+  const [loading, setLoading] = React.useState(true);
 
   //destructure anime
   const {
@@ -30,11 +32,31 @@ const Anime = () => {
     );
     const data = await response.json();
     setAnime(data);
-    console.log(data);
+  };
+
+  //get franchises based on id
+  const getFranchises = async (anime) => {
+    try {
+      const response = await fetch(
+        `https://api.anilibria.tv/v3.0/title?id=${anime}&filter=id,names,franchises,genres,description,season,status.string,type.full_string,player&remove=torrents`
+      );
+      const data = await response.json();
+
+      //Проверяем, не пуст ли массив "франшизы"
+      if (data.franchises && data.franchises.length > 0) {
+        setFranchise(data);
+        console.log(data);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false); // Устанавливаем значение false для загрузки независимо
+    }
   };
 
   useEffect(() => {
     getAnime(id);
+    getFranchises(id);
   }, [id]);
 
   return (
@@ -113,12 +135,42 @@ const Anime = () => {
               className="player"
               title="player"
               src={`https://www.anilibria.tv/public/iframe.php?id=${anime.id}`}
-              allow='fullscreen *'
+              allow="fullscreen; accelerometer; encrypted-media; gyroscope; picture-in-picture"
               frameBorder={0}
             ></iframe>
           </div>
         </DesignPlayer>
       </DesignOuterTitle>
+      {franchise?.franchises && franchise.franchises.length > 0 && (
+        <DesignOuterFranchises>
+          <>
+            <h2>Связанные релизы</h2>
+            {/* Переделать на SwiperJS */}
+            {!loading && franchise?.franchises[0]?.releases && (
+              <div className="swiperFranchises">
+                <div className="swiperWrapper">
+                  {franchise.franchises[0].releases.map((release) => (
+                    <SwiperSlide key={release.id}>
+                      <AnimeCardTitleLink
+                        to={`/anime/${release.id}`}
+                        onClick={() => {
+                          navigate(`/anime/${release.id}`);
+                        }}
+                        key={release.id}
+                      >
+                        <AnimeCardTitleImg
+                          src={`${posterUrl}${release.id}.webp`}
+                          alt={release.code}
+                        />
+                      </AnimeCardTitleLink>
+                    </SwiperSlide>
+                  ))}
+                </div>
+              </div>
+            )}
+          </>
+        </DesignOuterFranchises>
+      )}
     </DesignMain>
   );
 };
@@ -212,6 +264,72 @@ const DesignPlayer = styled.div`
     width: 100%;
     height: 550px;
   }
+`;
+const DesignOuterFranchises = styled.div`
+  background-color: #1d1d1dcc;
+  border-radius: 20px;
+  margin: 10px 20px;
+  max-width: 1400px;
+  padding: 20px 0;
+  text-align: center;
+  width: 100%;
+  h2 {
+    color: white;
+    font-size: 32px;
+    margin-bottom: 10px;
+    margin-top: 0;
+  }
+  .swiperFranchises {
+    touch-action: pan-y;
+    display: block;
+    list-style: none;
+    margin-left: auto;
+    margin-right: auto;
+    overflow: hidden;
+    overflow: clip;
+    padding: 0;
+    position: relative;
+    z-index: 1;
+  }
+  .swiperWrapper {
+    box-sizing: initial;
+    display: flex;
+    height: 100%;
+    position: relative;
+    width: 100%;
+    z-index: 1;
+  }
+`;
+const SwiperSlide = styled.div`
+  width: 200px;
+  margin-right: 40px;
+  display: block;
+  flex-shrink: 0;
+  height: 100%;
+  position: relative;
+`;
+const AnimeCardTitleLink = styled(Link)`
+  border-radius: 20px;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin: 20px 25px;
+  max-height: 100%;
+  max-width: 187px;
+  text-decoration: none;
+  transform: scale(1);
+  transition: transform 0.3s ease;
+  &:hover {
+    transform: scale(1.05);
+    transition: all 0.3s ease;
+  }
+`;
+const AnimeCardTitleImg = styled.img`
+  border-radius: 20px;
+  width: 100%;
+  height: 100%; // Allow the image to adjust its height
+  object-fit: cover;
 `;
 
 export default Anime;
